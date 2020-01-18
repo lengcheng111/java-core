e18979106575dfdd3718e0d8ad0510a12ed866c4
truonglv <truonglv174@gmail.com>
2020-01-18 15:16:33 +0700

    fix bai1

--- src/main/java/com/java/basic/bai1/Bai1.java
+++ src/main/java/com/java/basic/bai1/Bai1.java
@@ -1,40 +1,37 @@
 package com.java.basic.bai1;
 
-import java.util.Scanner;
+import java.util.Arrays;
 
 public class Bai1 {
 
 	public static void main(String[] args) {
-		
-		
+		final int[] arr = { 2, 11, 23, 44, 66, 3, 23, 5, 67, 8, 9 };
+		final int[] sorted = sort(arr);
+		System.out.println(Arrays.toString(sorted));
 	}
 
-	// VIET HAM NHAP VAO MOT MANG
-	public static void NhapMang(int arr[], int n) {
-		Scanner keyboard = new Scanner(System.in);
-		n = keyboard.nextInt();
-		for (int i = 0; i < n; i++) {
-			arr[i] = keyboard.nextInt();
-		}
-	}
-
-	// VIET HAM KIEM TRA SO NGUYEN TO
-	public static int kiemtra(int m) {
-		int dem;
-		for (int j = 2; j < m; j++) {
+	// check a number is prime or not
+	public static boolean isPrimeNumber(int m) {
+		for (int j = 2; j < Math.sqrt(m); j++) {
 			if (m % j == 0) {
-				dem = dem + 1;
+				return false;
 			}
-
 		}
-		if (dem == 0) {
-			return 1;
-		} else
-			return 0;
-	
+		return true;
 	}
-	// LOC RA CAC PHAN TU TRONG MANG LA SO NGUYEN TO
-	public static void LOC(int arr2[], int n2) {
-		
+
+	// sort prime in array
+	public static int[] sort(int arr[]) {
+		for (int i = 0; i < arr.length; i++) {
+			final boolean primeNumber = isPrimeNumber(arr[i]);
+			for (int j = i + 1; j < arr.length; j++) {
+				if (primeNumber && isPrimeNumber(arr[j]) && arr[i] > arr[j]) {
+					final int temp = arr[i];
+					arr[i] = arr[j];
+					arr[j] = temp;
+				}
+			}
+		}
+		return arr;
 	}
 }
