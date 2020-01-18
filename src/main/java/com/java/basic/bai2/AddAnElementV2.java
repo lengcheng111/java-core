1767da53706512e50ccdb1b13af9b31280be95b6
truonglv <truonglv174@gmail.com>
2020-01-18 16:44:01 +0700

    add customstack

--- src/main/java/com/java/basic/bai2/AddAnElementV2.java
+++ src/main/java/com/java/basic/bai2/AddAnElementV2.java
@@ -7,6 +7,11 @@ public class AddAnElementV2 {
 	public static void main(String[] args) {
 		CustomStack customStack = new CustomStack();
 		customStack.add(12);
+		customStack.add(3);
+		customStack.add(2);
+
+		customStack.pop();
+		customStack.peek();
 
 		customStack.print();
 
